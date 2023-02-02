cc09d656fb256629292e740da0611cffe1b801a4
gabrieltassinari <kievhotkatwo@gmail.com>
2023-02-02 16:02:52 -0300

    new route

--- app.py
+++ app.py
@@ -5,10 +5,18 @@ app = Flask(__name__)
 
 @app.route('/')
 def get_coin():
-    coin = requests.get(
+    coin_list = requests.get(
         'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd').json()
 
-    return render_template('main.html', coin=coin)
+    return render_template('main.html', coin=coin_list)
+
+@app.route('/coin/<crypto>')
+def info_coin(crypto):
+
+    coin = requests.get(
+    f'https://api.coingecko.com/api/v3/coins/{crypto}').json()
+
+    return coin
 
 if __name__ == "__main__":
     app.run()
