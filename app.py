46558df195ca967a71df8e0e58bb70ba5c887bb3
gabrieltassinari <kievhotkatwo@gmail.com>
2023-02-03 15:36:56 -0300

    coin information

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from datetime import datetime
 import requests
 
 app = Flask(__name__)
@@ -13,10 +14,20 @@ def get_coin():
 @app.route('/coin/<crypto>')
 def info_coin(crypto):
 
+    now = int(datetime.now().timestamp())
+
     coin = requests.get(
     f'https://api.coingecko.com/api/v3/coins/{crypto}').json()
 
-    return coin
+    price = requests.get(
+        f'https://api.coingecko.com/api/v3/coins/{crypto}/market_chart/range?vs_currency=usd&from={now - 604800}&to={now}').json()
+
+    return render_template('coin.html', coin=coin, price=price)
+
+@app.template_filter('ctime')
+def convert_time(t):
+    return datetime.fromtimestamp(t / 1000)
+
 
 if __name__ == "__main__":
     app.run()
